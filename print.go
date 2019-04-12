5a49dbf226b713b425098fb42c79c7f4d677b50f
WilliamYang1992 <505741310@qq.com>
2019-04-12 09:44:20 +0800

    feat: new func: `PrintJSON` and `MustPrintJSON`

--- print.go
+++ print.go
@@ -1,8 +1,11 @@
 package hatty
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+)
 
-// Transform to JSON, output as string
+// Transform to JSON, return string
 func ToJSON(v interface{}) (string, error) {
 	data, err := json.Marshal(v)
 	if err != nil {
@@ -11,12 +14,27 @@ func ToJSON(v interface{}) (string, error) {
 	return string(data), err
 }
 
-// Transform to JSON, output as string, if error occurred,
-// panic it
+// Transform to JSON, return string. panic it if error occurred
 func MustToJSON(v interface{}) string {
 	data, err := json.Marshal(v)
 	if err != nil {
 		panic(err)
 	}
 	return string(data)
+}
+
+// Print JSON to Stdout
+func PrintJSON(v interface{}) error {
+	data, err := json.Marshal(v)
+	fmt.Println(string(data))
+	return err
+}
+
+// Print JSON to Stdout, panic it if error occurred
+func MustPrintJSON(v interface{}) {
+	data, err := json.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(data))
 }
